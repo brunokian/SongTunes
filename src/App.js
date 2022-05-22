a5d43bd66a234f323401ce51ffefda232b3387bc
brunokian <brunokian@hotmail.com>
2022-05-22 04:34:18 -0300

    requisito 2

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import { BrowserRouter, Route, Switch, Redirect } from 'react-router-dom';
 
 import Login from './Login';
 import Search from './Search';
@@ -9,12 +9,77 @@ import Profile from './Profile';
 import ProfileEdit from './ProfileEdit';
 import NotFound from './NotFound';
 
+import { createUser } from './services/userAPI';
+
 class App extends React.Component {
+  state = {
+    login: '',
+    loginValidation: true,
+    hasLogged: false,
+    load: false,
+  }
+
+  validation = () => {
+    const { login } = this.state;
+    const minimumCaracteres = 3;
+    if (login.length < minimumCaracteres) {
+      this.setState({ loginValidation: true });
+    } else {
+      this.setState({ loginValidation: false });
+    }
+  }
+
+  logging = async () => {
+    const { login } = this.state;
+    this.setState(
+      { load: true },
+      async () => {
+        await createUser({ name: login });
+        this.setState({
+          hasLogged: true,
+          load: false });
+      },
+    );
+  }
+
+  handleChange = ({ target }) => {
+    const { name } = target;
+    const value = target.type === 'checkbox' ? target.checked : target.value;
+    this.setState({ [name]: value }, this.validation);
+  }
+
   render() {
+    const { login, loginValidation, hasLogged, load } = this.state;
+
     return (
       <BrowserRouter>
         <Switch>
-          <Route exact path="/" component={ Login } />
+          {hasLogged ? <Redirect to="/search" /> : <Route
+            exact
+            path="/"
+            render={ (props) => (<Login
+              { ...props }
+              login={ login }
+              loginValidation={ loginValidation }
+              handleChange={ this.handleChange }
+              hasLogged={ hasLogged }
+              logging={ this.logging }
+              load={ load }
+            />) }
+          />}
+          {/* <Route
+            exact
+            path="/"
+            render={ (props) => (<Login
+              { ...props }
+              login={ login }
+              loginValidation={ loginValidation }
+              handleChange={ this.handleChange }
+              hasLogged={ hasLogged }
+              logging={ this.logging }
+              load={ load }
+            />) }
+          /> */}
           <Route path="/search" component={ Search } />
           <Route path="/album/:id" component={ Album } />
           <Route path="/favorites" component={ Favorites } />
--- src/Login.jsx
+++ src/Login.jsx
@@ -1,13 +1,48 @@
 import React from 'react';
+import PropTypes from 'prop-types';
+import Loading from './Loading';
+
+// import { createUser } from './services/userAPI';
 
 class Login extends React.Component {
   render() {
+    const { login, handleChange, logging, loginValidation, load } = this.props;
+    console.log(logging);
+    if (load) {
+      return <Loading />;
+    }
     return (
       <div data-testid="page-login">
         <p>COMPONENTE LOGIN</p>
+        <input
+          id="login"
+          name="login"
+          type="text"
+          value={ login }
+          onChange={ handleChange }
+          data-testid="login-name-input"
+        />
+        <button
+          id="loginButton"
+          name="loginButton"
+          type="submit"
+          disabled={ loginValidation }
+          onClick={ () => logging({ name: login }) }
+          data-testid="login-submit-button"
+        >
+          Entrar
+        </button>
       </div>
     );
   }
 }
+
+Login.propTypes = {
+  login: PropTypes.string.isRequired,
+  loginValidation: PropTypes.bool.isRequired,
+  handleChange: PropTypes.func.isRequired,
+  logging: PropTypes.func.isRequired,
+  load: PropTypes.bool.isRequired,
+};
 
 export default Login;
