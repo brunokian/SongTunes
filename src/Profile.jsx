36bcbd22407cc0261b44ff41f74c0b99dffd449d
brunokian <brunokian@hotmail.com>
2022-05-31 19:51:37 -0300

    requisito 13

--- src/Profile.jsx
+++ src/Profile.jsx
@@ -1,15 +1,36 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import Header from './components/Header';
+import { getUser } from './services/userAPI';
 
 class Profile extends React.Component {
-  render() {
-    return (
-      <div data-testid="page-profile">
-        <Header />
-        <p>PROFILE CONTEUDO</p>
-      </div>
-    );
+  state = {
+    userData: {},
   }
+
+  componentDidMount = async () => {
+    this.userProfile();
+  }
+
+userProfile = async () => {
+  const result = await getUser();
+  this.setState({ userData: result });
+}
+
+render() {
+  const { userData } = this.state;
+  return (
+    <div data-testid="page-profile">
+      <Header />
+      <p>PROFILE CONTEUDO</p>
+      <p>{ userData.name }</p>
+      <p>{ userData.email }</p>
+      <p>{ userData.description }</p>
+      <img src={ userData.image } alt={ userData.name } data-testid="profile-image" />
+      <Link to="/profile/edit">Editar perfil</Link>
+    </div>
+  );
+}
 }
 
 export default Profile;
